e74302870e82cbef58e5122fa9fb2fb4914fe127
Aaron Berliner <aaron.berliner@berkeley.edu>
2020-05-29 20:54:52 -0700

    Updating functions

--- rs_funcs.py
+++ rs_funcs.py
@@ -240,11 +240,11 @@ def call_mcd(req,lat,lon,ls,hr,scenario=1):
     pres = vals[0] * 1e-2
     temp = vals[1]
     air = (vals[0]/(K*vals[1]))*1e-6
-    O3_pp = vals[2] * air
-    O2_pp = vals[3] * air
-    CO2_pp = vals[4] * air
-    H2O_pp = vals[5] * air
-    NO2_pp = vals[4] * 0
+    pp_O3 = vals[2] * air
+    pp_O2 = vals[3] * air
+    Cpp_O2 = vals[4] * air
+    pp_H2O = vals[5] * air
+    Npp_O2 = vals[4] * 0
     reff_ice = vals[6] * 1e6
     iwc = air * vals[7] * (18/Av) * 1e3
     reff_dust = vals[8] * 1e6
@@ -256,7 +256,7 @@ def call_mcd(req,lat,lon,ls,hr,scenario=1):
 
     ## Format as dictionary and return values
     keys = ['alt_datum','pressure','temperature','air_density','pp_O3','pp_O2','pp_H2O','pp_CO2','pp_NO2','content_dust','reff_dust','content_ice','reff_ice','flux_dw_sw','flux_dw_lw','flux_uw_sw','flux_uw_lw']
-    vals = [alt, pres, temp, air, O3_pp, O2_pp, H2O_pp, CO2_pp, NO2_pp, dust_conc, reff_dust, iwc, reff_ice, flux_dw_sw, flux_dw_lw, flux_uw_sw, flux_uw_lw]
+    vals = [alt_datum, pres, temp, air, pp_O3, pp_O2, pp_H2O, pp_O2, pp_O2, dust_conc, reff_dust, iwc, reff_ice, flux_dw_sw, flux_dw_lw, flux_uw_sw, flux_uw_lw]
 
     return dict(zip(keys,vals))
 
@@ -328,3 +328,204 @@ def calc_E(M,e=0.0934,n=1000):
     for k in range(0,n):
         E = M + e*np.sin(E)
     return E
+
+# def parse_netcdf_by_index(inds,filename_netCDF='Initial_Grid.nc'):
+#
+#     ## Specify indices
+#     # ind_lat = inds[1]
+#     # ind_lon = inds[2]
+#     # ind_hr = inds[3]
+#     # ind_ls = inds[0]
+#     d = dict(ls=inds[0], lat=inds[1], lon=inds[2], hr=inds[3])
+#     ## load data from netcdf with dask and xarray
+#     ds = xr.open_dataset(filename_netCDF, decode_cf=False)
+#     ds = ds[d]
+#     l = list(ds.keys())
+#     for i in range(0,len(l)):
+#         exec('%s=ds["%s"].values' % (l[i], l[i]))
+#     args = [pressure,temperature,air_density,pp_O3,pp_O2,pp_CO2,pp_H2O,alt_datum,reff_ice,content_ice,reff_dust,content_dust,flux_dw_sw,irr_TOA,lambdaa,albedo]
+#     return args
+
+def paralell_lrt(inds=[0,10,10,6], filename_netCDF='Initial_Grid.nc'):
+
+    ## load data from netcdf with dask and xarray
+    d = dict(ls=inds[0], lat=inds[1], lon=inds[2], hr=inds[3], latitude=inds[1], longitude=inds[2])
+    ds = xr.open_dataset(filename_netCDF, decode_cf=False)
+    ds = ds[d]
+    # l = list(ds.keys())
+    # for i in range(0,len(l)):
+    #     print(l[i])
+    #     eval('%s=ds["%s"].values' % (l[i], l[i]))
+
+    # irr_TOA = irr_TOA*1e3
+    vals = 0
+    atp_set = 3
+    try:
+        id = '__'+id_generator(size=10)
+        ind_lat = inds[1]
+        ind_lon = inds[2]
+        ind_hr = inds[3]
+        ind_ls = inds[0]
+        atp = 0
+        while atp < atp_set:
+            if ds['flux_dw_sw'].values > 0:
+                if np.sum(ds['irr_TOA'].values*1e3) > 0:
+                    write_atmos(ds['alt_datum'].values,ds['pressure'].values,ds['temperature'].values,ds['air_density'].values, ds['pp_O3'].values, ds['pp_O2'].values, ds['pp_H2O'].values, ds['pp_O2'].values, ds['pp_O2'].values*0.0, filename=id)
+#                     print(flux_dw_sw)
+                    write_flux(ds['wl'].values, ds['irr_TOA'].values*1e3, filename=id)
+                    write_cloud(ds['alt_datum'].values, ds['content_ice'].values,ds['reff_ice'].values, filename=id)
+                    write_dust(ds['alt_datum'].values, ds['content_dust'].values, ds['reff_dust'].values, filename=id)
+                    write_input(albedo=ds['albedo'].values, datum=ds['alt_datum'].values[0]+.001, id=id)
+                    libRadtran_return = call_libRadtran(id=id)
+                    if libRadtran_return[1] == 0:
+                        vals = read_libRadtran(id=id)
+                        [lambdaa, edir, eglo, edn, eup, enet, esum] = vals
+                        [edir, eglo, edn, eup, enet, esum] = [edir*1e6, eglo*1e6, edn*1e6, eup*1e6, enet*1e6, esum*1e6]
+                        mcd_lrt_error = calc_mcd_lrt_error(lambdaa*1e-9,eglo,ds['flux_dw_sw'].values)
+                        vals = SQlim_1bg(np.array(lambdaa)*1e-9, np.array(eglo), ds['temperature'].values[0])
+                        [j1_bg_vec,j1_etaPV,j1_etaPEC_H2,j1_etaPEC_NH3,j1_etaPEC_AA, j1_max_etaPV,j1_max_etaPEC_H2,j1_max_etaPEC_NH3,j1_max_etaPEC_AA,j1_bg_PVmax,j1_bg_H2max,j1_bg_NH3max,j1_bg_AAmax] = vals
+                        vals = SQlim_2bg(np.array(lambdaa)*1e-9, np.array(eglo), ds['temperature'].values[0])
+                        [j2_bg1_vec,j2_bg2_vec,j2_etaPV_2bg,j2_etaPEC_H2_2bg,j2_etaPEC_NH3_2bg,j2_etaPEC_AA_2bg, j2_max_etaPV_2bg,j2_max_etaPEC_H2_2bg,j2_max_etaPEC_NH3_2bg,j2_max_etaPEC_AA_2bg, j2_bg1_PVmax,j2_bg2_PVmax,j2_bg1_H2max,j2_bg2_H2max,j2_bg1_NH3max,j2_bg2_NH3max,j2_bg1_AAmax,j2_bg2_AAmax] = vals
+                        vals = SQlim_3bg(np.array(lambdaa)*1e-9, np.array(eglo), ds['temperature'].values[0])
+                        [j3_bg1_vec,j3_bg2_vec,j3_bg3_vec,j3_etaPV_3bg,j3_max_etaPV_3bg,j3_bg1max,j3_bg2max,j3_bg3max,j3_J_bg1,j3_J_bg2,j3_J_bg3,j3_v] = vals
+                        vals = 0
+                        vals = [lambdaa, j1_bg_vec, j2_bg1_vec, j2_bg2_vec, j3_bg1_vec, j3_bg2_vec, j3_bg3_vec, id, edir, edn, eup, eglo, mcd_lrt_error, j1_etaPV,j1_etaPEC_H2,j1_etaPEC_NH3,j1_etaPEC_AA, j1_max_etaPV,j1_max_etaPEC_H2,j1_max_etaPEC_NH3,j1_max_etaPEC_AA,j1_bg_PVmax,j1_bg_H2max,j1_bg_NH3max,j1_bg_AAmax,j2_etaPV_2bg,j2_etaPEC_H2_2bg,j2_etaPEC_NH3_2bg,j2_etaPEC_AA_2bg, j2_max_etaPV_2bg,j2_max_etaPEC_H2_2bg,j2_max_etaPEC_NH3_2bg,j2_max_etaPEC_AA_2bg, j2_bg1_PVmax,j2_bg2_PVmax,j2_bg1_H2max,j2_bg2_H2max,j2_bg1_NH3max,j2_bg2_NH3max,j2_bg1_AAmax,j2_bg2_AAmax,j3_etaPV_3bg,j3_max_etaPV_3bg,j3_bg1max,j3_bg2max,j3_bg3max,j3_J_bg1,j3_J_bg2,j3_J_bg3]
+                        cleaner(id=id)
+#                         if atp > 0:
+#                             print('Passed ' + str(inds)+ ' after ' + str(atp) + ' attemps')
+                        return inds,vals
+                    else:
+                        atp = atp + 1
+#                         print(str(atp) + ' LRT Error at ' +  str(inds))
+                        time.sleep(3)
+                else:
+                    return None
+            else:
+                return None
+    except Exception:
+        traceback.print_exc()
+        cleaner(id=id)
+    print('total fail at ' + str(inds))
+    return None
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    '''Function produces an ID string of a size [default=6]'''
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def cleaner(id='XXXXXX', directory=''):
+    dirList = os.listdir()   # Use os.listdir() if want current directory
+    dirList.sort()
+    for i in dirList:
+        if id in str(i):
+            os.remove(i)
+
+def calc_mcd_lrt_error(lambdaa,spec_flux,surf_flux):
+    int_spec_flux = np.trapz(spec_flux, x=lambdaa)
+    # print('mcd flux: ' + str(surf_flux))
+    # print('lrt flux: ' + str(int_spec_flux))
+    error = np.abs(int_spec_flux - surf_flux)/surf_flux
+    return error
+
+def save_singlepoint_netcdf(inds,vals,directory='singlePoints/'):
+    [wl, j1_bg, j2_bg1, j2_bg2, j3_bg1, j3_bg2, j3_bg3, id, edir, edn, eup, eglo, mcd_lrt_error, j1_etaPV,j1_etaPEC_H2,j1_etaPEC_NH3,j1_etaPEC_AA, j1_max_etaPV,j1_max_etaPEC_H2,j1_max_etaPEC_NH3,j1_max_etaPEC_AA,j1_bg_PVmax,j1_bg_H2max,j1_bg_NH3max,j1_bg_AAmax,j2_etaPV_2bg,j2_etaPEC_H2_2bg,j2_etaPEC_NH3_2bg,j2_etaPEC_AA_2bg, j2_max_etaPV_2bg,j2_max_etaPEC_H2_2bg,j2_max_etaPEC_NH3_2bg,j2_max_etaPEC_AA_2bg, j2_bg1_PVmax,j2_bg2_PVmax,j2_bg1_H2max,j2_bg2_H2max,j2_bg1_NH3max,j2_bg2_NH3max,j2_bg1_AAmax,j2_bg2_AAmax,j3_etaPV_3bg,j3_max_etaPV_3bg,j3_bg1max,j3_bg2max,j3_bg3max,j3_J_bg1,j3_J_bg2,j3_J_bg3] = vals
+
+    encodingDict = {
+        'edir': {'zlib': True,  '_FillValue':0.0},
+        'edn': {'zlib': True,  '_FillValue':0.0},
+        'eup': {'zlib': True,  '_FillValue':0.0},
+        'eglo': {'zlib': True,  '_FillValue':0.0},
+        'mcd_lrt_error': {'zlib': True,  '_FillValue':0.0},
+        'j1_etaPV': {'zlib': True,  '_FillValue':0.0},
+        'j1_etaPEC_H2': {'zlib': True,  '_FillValue':0.0},
+        'j1_etaPEC_NH3': {'zlib': True,  '_FillValue':0.0},
+        'j1_etaPEC_AA': {'zlib': True,  '_FillValue':0.0},
+        'j1_max_etaPV': {'zlib': True,  '_FillValue':0.0},
+        'j1_max_etaPEC_H2': {'zlib': True,  '_FillValue':0.0},
+        'j1_max_etaPEC_NH3': {'zlib': True,  '_FillValue':0.0},
+        'j1_max_etaPEC_AA': {'zlib': True,  '_FillValue':0.0},
+        'j1_bg_PVmax': {'zlib': True,  '_FillValue':0.0},
+        'j1_bg_H2max': {'zlib': True,  '_FillValue':0.0},
+        'j1_bg_NH3max': {'zlib': True,  '_FillValue':0.0},
+        'j1_bg_AAmax': {'zlib': True,  '_FillValue':0.0},
+        'j2_etaPV_2bg': {'zlib': True,  '_FillValue':0.0},
+        'j2_etaPEC_H2_2bg': {'zlib': True,  '_FillValue':0.0},
+        'j2_etaPEC_NH3_2bg': {'zlib': True,  '_FillValue':0.0},
+        'j2_etaPEC_AA_2bg': {'zlib': True,  '_FillValue':0.0},
+        'j2_max_etaPV_2bg': {'zlib': True,  '_FillValue':0.0},
+        'j2_max_etaPEC_H2_2bg': {'zlib': True,  '_FillValue':0.0},
+        'j2_max_etaPEC_NH3_2bg': {'zlib': True,  '_FillValue':0.0},
+        'j2_max_etaPEC_AA_2bg': {'zlib': True,  '_FillValue':0.0},
+        'j2_bg1_PVmax': {'zlib': True,  '_FillValue':0.0},
+        'j2_bg2_PVmax': {'zlib': True,  '_FillValue':0.0},
+        'j2_bg1_H2max': {'zlib': True,  '_FillValue':0.0},
+        'j2_bg2_H2max': {'zlib': True,  '_FillValue':0.0},
+        'j2_bg1_NH3max': {'zlib': True,  '_FillValue':0.0},
+        'j2_bg2_NH3max': {'zlib': True,  '_FillValue':0.0},
+        'j2_bg1_AAmax': {'zlib': True,  '_FillValue':0.0},
+        'j2_bg2_AAmax': {'zlib': True,  '_FillValue':0.0},
+        'j3_etaPV_3bg': {'zlib': True,  '_FillValue':0.0},
+        'j3_max_etaPV_3bg': {'zlib': True,  '_FillValue':0.0},
+        'j3_bg1max': {'zlib': True,  '_FillValue':0.0},
+        'j3_bg2max': {'zlib': True,  '_FillValue':0.0},
+        'j3_bg3max': {'zlib': True,  '_FillValue':0.0},
+        'j3_J_bg1': {'zlib': True,  '_FillValue':0.0},
+        'j3_J_bg2': {'zlib': True,  '_FillValue':0.0},
+        'j3_J_bg3': {'zlib': True,  '_FillValue':0.0}
+    }
+    d = {
+        'wl': {'dims': ('wl'), 'data': wl},
+        'j1-bg': {'dims': ('j1-bg'), 'data': j1_bg},
+        'j2-bg1': {'dims': ('j2-bg1'), 'data': j2_bg1},
+        'j2-bg2': {'dims': ('j2-bg2'), 'data': j2_bg2},
+        'j3-bg1': {'dims': ('j3-bg1'), 'data': j3_bg1},
+        'j3-bg2': {'dims': ('j3-bg2'), 'data': j3_bg2},
+        'j3-bg3': {'dims': ('j3-bg3'), 'data': j3_bg3},
+        'lat': {'dims': (), 'data': inds[1]},
+        'lon': {'dims': (), 'data': inds[2]},
+        'ls': {'dims': (), 'data': inds[0]},
+        'hr': {'dims': (), 'data': inds[3]},
+        'edir': {'dims': ('wl'), 'data': edir},
+        'edn': {'dims': ('wl'), 'data': edn},
+        'eup': {'dims': ('wl'), 'data': eup},
+        'eglo': {'dims': ('wl'), 'data': eglo},
+        'mcd_lrt_error': {'dims': (), 'data': mcd_lrt_error},
+        'j1_etaPV': {'dims': ('j1-bg'), 'data': j1_etaPV},
+        'j1_etaPEC_H2': {'dims': ('j1-bg'), 'data': j1_etaPEC_H2},
+        'j1_etaPEC_NH3': {'dims': ('j1-bg'), 'data': j1_etaPEC_NH3},
+        'j1_etaPEC_AA': {'dims': ('j1-bg'), 'data': j1_etaPEC_AA},
+        'j1_max_etaPV': {'dims': (), 'data': j1_max_etaPV},
+        'j1_max_etaPEC_H2': {'dims': (), 'data': j1_max_etaPEC_H2},
+        'j1_max_etaPEC_NH3': {'dims': (), 'data': j1_max_etaPEC_NH3},
+        'j1_max_etaPEC_AA': {'dims': (), 'data': j1_max_etaPEC_AA},
+        'j1_bg_PVmax': {'dims': (), 'data': j1_bg_PVmax},
+        'j1_bg_H2max': {'dims': (), 'data': j1_bg_H2max},
+        'j1_bg_NH3max': {'dims': (), 'data': j1_bg_NH3max},
+        'j1_bg_AAmax': {'dims': (), 'data': j1_bg_AAmax},
+        'j2_etaPV_2bg': {'dims': ('j2-bg1','j2-bg2'), 'data': j2_etaPV_2bg},
+        'j2_etaPEC_H2_2bg': {'dims': ('j2-bg1','j2-bg2'), 'data': j2_etaPEC_H2_2bg},
+        'j2_etaPEC_NH3_2bg': {'dims': ('j2-bg1','j2-bg2'), 'data': j2_etaPEC_NH3_2bg},
+        'j2_etaPEC_AA_2bg': {'dims': ('j2-bg1','j2-bg2'), 'data': j2_etaPEC_AA_2bg},
+        'j2_max_etaPV_2bg': {'dims': (), 'data': j2_max_etaPV_2bg},
+        'j2_max_etaPEC_H2_2bg': {'dims': (), 'data': j2_max_etaPEC_H2_2bg},
+        'j2_max_etaPEC_NH3_2bg': {'dims': (), 'data': j2_max_etaPEC_NH3_2bg},
+        'j2_max_etaPEC_AA_2bg': {'dims': (), 'data': j2_max_etaPEC_AA_2bg},
+        'j2_bg1_PVmax': {'dims': (), 'data': j2_bg1_PVmax},
+        'j2_bg2_PVmax': {'dims': (), 'data': j2_bg2_PVmax},
+        'j2_bg1_H2max': {'dims': (), 'data': j2_bg1_H2max},
+        'j2_bg2_H2max': {'dims': (), 'data': j2_bg2_H2max},
+        'j2_bg1_NH3max': {'dims': (), 'data': j2_bg1_NH3max},
+        'j2_bg2_NH3max': {'dims': (), 'data': j2_bg2_NH3max},
+        'j2_bg1_AAmax': {'dims': (), 'data': j2_bg1_AAmax},
+        'j2_bg2_AAmax': {'dims': (), 'data': j2_bg2_AAmax},
+        'j3_etaPV_3bg': {'dims': ('j3-bg1','j3-bg2','j3-bg3'), 'data': j3_etaPV_3bg},
+        'j3_max_etaPV_3bg': {'dims': (), 'data': j3_max_etaPV_3bg},
+        'j3_bg1max': {'dims': (), 'data': j3_bg1max},
+        'j3_bg2max': {'dims': (), 'data': j3_bg2max},
+        'j3_bg3max': {'dims': (), 'data': j3_bg3max}}
+
+    test = xr.Dataset.from_dict(d)
+    test.to_netcdf(directory+id+'_singlePoint.nc')
+
+
+x = paralell_lrt(inds=[0,10,10,6], filename_netCDF='Initial_Grid.nc')
+save_singlepoint_netcdf(x[0],x[1],directory='')
